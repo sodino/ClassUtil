a625ec3fd4de2cc5b3061392631aedb61433e4a2
sodino <sodino@qq.com>
2014-02-10 21:24:18 +0800

    代码初实现，提交了

---
+++ src/lab/sodino/clazz/ClassTestDemo.java
@@ -0,0 +1,9 @@
+package lab.sodino.clazz;
+/**
+ * @author Sodino E-mail:sodino@qq.com
+ * @version Time：2014年2月10日 下午9:06:55
+ */
+@author(name="sodino", webSite="sodino.com")
+public class ClassTestDemo {
+
+}
